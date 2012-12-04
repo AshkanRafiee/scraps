ed8a247b3d256c6649900a638de21b5cc837c7f0
Enno Weichert <enno.weichert@gmail.com>
2012-12-04 15:54:14 +0100

    add start-program-quietly.s

---
+++ bash/start-program-quietly.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -i
+
+if [ $# -gt 0 ] ; then
+	# Expand if $1 is an alias
+	if [ $(alias -p | awk -F "[ =]" '{print $2}' | grep -x $1) > 0 ] ; then
+		set -- $(alias $1 | awk -F "['']" '{print $2}') "${@:2}"
+	fi
+	($@ &) &>/dev/null
+else
+	echo "missing argument"
+fi
