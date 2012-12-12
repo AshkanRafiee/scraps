70aa82b4013bb6c7aa3fa808a5a2d0359ad36800
Enno Weichert <enno.weichert@gmail.com>
2012-12-12 13:47:43 +0100

    Implement most of the functionality

--- bash/unzip-unicode.sh
+++ bash/unzip-unicode.sh
@@ -1,9 +1,38 @@
 # Requires convmv, unzip 6.00 (UTF support)
 # TODO: Honor all original unzip options as well
 
+################################################################################
+
+#
+# Set defaults here
+#
+
+SOURCE_LOCALE=ja_JP
+SOURCE_CODEPAGE=shiftjis
+
+BIN_UNZIP=/usr/bin/unzip
+BIN_CONVMV=/usr/bin/convmv
+
+################################################################################
+
+#
+# Don't change things beyond this point unless you know what you are doing!
+#
+
+SOURCE_FILENAME=
+TARGET_DIRECTORY=.
+TEMP_DIRECTORY=$(mktemp -d $(basename $0).XXXXXX)
+
+cleanup()
+{
+rm -rf $TEMP_DIRECTORY
+}
+
 usage()
 {
-cat << EOF
+cleanup
+
+cat <<EOF
 Usage: $0 [<options> ...] <filename>
 
 Extract files honoring filename encodings, then convert them to UTF-8.
@@ -12,19 +41,14 @@ You can set the default behaviour within the script.
 OPTIONS:
    -h              Show this message
    -d <directory>  Target directory
-   -l <locale>     Language environment
-   -f <codepage>   Source filename encoding
+   -l <locale>     Source locale (e.g. ja_JP)
+   -c <codepage>   Source codepage (e.g. shiftjis)
 EOF
 }
 
-FILENAME=
-TARGETDIR=.
-LANGENV=ja_JP
-FROMENCODE=shiftjis
+# Parse arguments
 
-TMPDIR=$(mktemp -d $(basename $0).XXXXXX)
-
-while getopts “hd:l:f:v” OPTION
+while getopts “hd:l:c:v” OPTION
 do
     case $OPTION in
         h)
@@ -32,46 +56,53 @@ do
             exit 1
             ;;
         d)
-         	TARGETDIR=$OPTARG
+         	TARGET_DIRECTORY=$OPTARG
              ;;
         l)
-            LANGENV=$OPTARG
+            SOURCE_LOCALE=$OPTARG
             ;;
-        f)
-            FROMENCODE=$OPTARG
+        c)
+            SOURCE_CODEPAGE=$OPTARG
             ;;
         v)
             VERBOSE=1
             ;;
         ?)
-            usage
+            echo Try \`$0 -h\` for more information.
             exit
             ;;
     esac
 done
 
+if [[ -z $TARGET_DIRECTORY ]] || [[ -z $SOURCE_LOCALE ]] || [[ -z $SOURCE_CODEPAGE ]]; then
+    echo $0: option requires an argument
+    echo Try \`$0 -h\` for more information.
+    exit 1
+fi
+
 # The remaining argument is treated as the operand <filename>
 
 shift $((OPTIND-1))
 
-# now do something with $@
-# check if multiple arguments (it's probably more sane to only do one filename at a time) -> help
-# check if empty -> help
-FILENAME=$@
-
-if [[ -z $TARGETDIR ]] || [[ -z $LANGENV ]] || [[ -z $FROMENCODE ]]
-then
-    usage
+if [ $# = 1 ]; then
+    SOURCE_FILENAME=$@
+else
+    echo $0: requires one file operand
+    echo Try \`$0 -h\` for more information.
     exit 1
 fi
 
-echo $FILENAME
-echo $TARGETDIR
-echo $LANGENV
-echo $FROMENCODE
-echo $TMPDIR
+LANG=$SOURCE_LOCALE $BIN_UNZIP $SOURCE_FILENAME -d $TEMP_DIRECTORY
+$BIN_CONVMV -r -f $SOURCE_CODEPAGE -t uft8 $TEMP_DIRECTORY
+read -p "Does this look right to you?? " -n 1
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    cleanup
+    exit 1
+fi
 
-#LANG=$LANGENV unzip $FILENAME -d $TMPDIR/$TARGETDIR
-#convmv --notest -r -f $FROMENCODE -t uft8 $TMPDIR/
-#mv $TMPDIR/* $TMPDIR/.* .
-rmdir $TMPDIR
+$BIN_CONVMV --notest -r -f $SOURCE_CODEPAGE -t uft8 $TEMP_DIRECTORY
+
+mv $TEMP_DIRECTORY/* $TEMP_DIRECTORY/.* $TARGET_DIRECTORY
+
+cleanup
