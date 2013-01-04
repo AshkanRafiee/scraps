42851434269282bbfb2275d595b89ee96eb6293b
Enno Weichert <enno.weichert@gmail.com>
2013-01-04 15:19:39 +0100

    Use correct comparison operator

--- bash/start-program-quietly.sh
+++ bash/start-program-quietly.sh
@@ -2,7 +2,7 @@
 
 if [ $# -gt 0 ] ; then
 	# Expand if $1 is an alias
-	if [ $(alias -p | awk -F "[ =]" '{print $2}' | grep -x $1) > 0 ] ; then
+	if [ $(alias -p | awk -F "[ =]" '{print $2}' | grep -x $1) -gt 0 ] ; then
 		set -- $(alias $1 | awk -F "['']" '{print $2}') "${@:2}"
 	fi
 	($@ &) &>/dev/null
