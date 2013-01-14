fc4c24d95d7edb42b15d7ea86554d988c803566b
Enno Weichert <enno.weichert@gmail.com>
2013-01-14 12:55:59 +0100

    Change whitespace, make condition explicit in hopes of being more POSIX stable

--- bash/start-program-quietly.sh
+++ bash/start-program-quietly.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -i
 
 if [ $# -gt 0 ] ; then
-	# Expand if $1 is an alias
-	ALIAS=$(alias $1 2>/dev/null | awk -F "['']" '{print $2}')
-	if [ "$ALIAS" ] ; then
-		set -- $ALIAS "${@:2}"
-	fi
-	($@ &) &>/dev/null
+    # Expand if $1 is an alias
+    ALIAS=$(alias $1 2>/dev/null | awk -F "['']" '{print $2}')
+    if [ -n "$ALIAS" ] ; then
+        set -- $ALIAS "${@:2}"
+    fi
+    ($@ &) &>/dev/null
 else
-	echo "missing argument"
+    echo "missing argument"
 fi
