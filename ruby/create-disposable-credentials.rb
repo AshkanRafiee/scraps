0cbe42c6dd9331265859161c5cd5c33348957e87
Enno Weichert <enno.weichert@gmail.com>
2014-01-17 15:43:50 +0100

    Reformat code whitespace

--- ruby/create-disposable-credentials.rb
+++ ruby/create-disposable-credentials.rb
@@ -6,9 +6,8 @@ firstname = Forgery::Name.first_name
 lastname  = Forgery::Name.last_name
 username  = (firstname[0] + lastname + rand(1000...9999).to_s).downcase
 password  = Forgery::Basic.password(:at_least => 8, :at_most => 8)
-
 email     = username + "@mailinator.com"
-inbox   = "http://mailinator.com/inbox.jsp?to=" + username
+inbox     = "http://mailinator.com/inbox.jsp?to=" + username
 
 puts "First Name:    " + firstname
 puts "Last Name:     " + lastname
