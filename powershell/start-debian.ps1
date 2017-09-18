14e195fd2b012b51ba9d1b74c6f047cd538fd3dc
Enno Weichert <ew@ennoweichert.com>
2017-09-18 15:16:01 +0200

    Clean up

--- powershell/start-debian.ps1
+++
@@ -1,3 +0,0 @@
-start-process ${env:ProgramFiles}'\oracle\virtualbox\vboxheadless' '-s Debian' -WindowStyle Hidden
-start-sleep -s 9
-start-process ${env:ProgramFiles}'\PuTTY\putty.exe' '-load "Debian Virtualbox Serial"' -verb runAs
